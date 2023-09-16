6831b20918f759cd42d36d035850afd6f0450098
shchoon <acckr0604@gmail.com>
2023-09-16 16:36:58 +0900

    feature/b 자기소개 구현

---
+++ src/Component1.js
@@ -0,0 +1,15 @@
+function Component1() {
+
+    return(
+        <div>
+            <h2>자기소개</h2>
+            <ol>
+                <li>이름: </li>
+                <li>나이: </li>
+                <li>성별: </li>
+            </ol>
+        </div>
+    )
+}
+
+export default Component1;
