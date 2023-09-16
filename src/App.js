d584cb72dea4756ab14e2f2f7b06a60378086594
shchoon <acckr0604@gmail.com>
2023-09-16 16:06:33 +0900

    add feature/a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  let [num, setNum] = useState(1);
+
   return (
     <div className="App">
       <h1>Hello</h1>
-      <button>Button</button>
+      <button onClick={() => {
+        setNum(num + 1);
+      }} >Button</button>
+
+      <div>{num}</div>
     </div>
   );
 }
