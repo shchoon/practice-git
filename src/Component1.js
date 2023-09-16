cda71a6bfb3deafc2485300e6c2fc0f958087347
shchoon <acckr0604@gmail.com>
2023-09-16 20:57:12 +0900

    feature/b add name
    feature/b add age
    
    feature/b add gender

--- src/Component1.js
+++ src/Component1.js
@@ -4,9 +4,9 @@ function Component1(props) {
         <div>
             <h2>자기소개</h2>
             <ol>
-                <li>이름: {props.name} </li>
-                <li>나이: </li>
-                <li>성별: </li>
+                <li>이름: {props.info.name}</li>
+                <li>나이: {props.info.age}</li>
+                <li>성별: {props.info.gender}</li>
             </ol>
         </div>
     )
